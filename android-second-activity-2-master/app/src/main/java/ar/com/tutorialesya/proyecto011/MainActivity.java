90acbe7307c76f2e887b3bfb425ac453fe533a0a
zigorlopezsanpelayo <zigorlopezsanpelayo@MacBook-Air-de-Zigor.local>
2017-02-07 18:45:44 +0100

    Second activity

---
+++ android-second-activity-2-master/app/src/main/java/ar/com/tutorialesya/proyecto011/MainActivity.java
@@ -0,0 +1,59 @@
+package ar.com.tutorialesya.proyecto011;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private EditText et1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1=(EditText)findViewById(R.id.editText);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void verificar(View v) {
+        String clave=et1.getText().toString();
+        if (clave.equals("abc123")) {
+            Intent i = new Intent(this, Actividad2.class);
+            startActivity(i);
+        }
+        else {
+            Toast notificacion= Toast.makeText(this,"Clave incorrecta",Toast.LENGTH_LONG);
+            notificacion.show();
+        }
+    }
+}
