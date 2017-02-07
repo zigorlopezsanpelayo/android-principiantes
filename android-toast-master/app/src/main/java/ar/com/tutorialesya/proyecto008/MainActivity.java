680508ee5abfecd00f64323b5860769a110f8603
zigorlopezsanpelayo <zigorlopezsanpelayo@MacBook-Air-de-Zigor.local>
2017-02-07 17:44:00 +0100

    Primer commit

---
+++ android-toast-master/app/src/main/java/ar/com/tutorialesya/proyecto008/MainActivity.java
@@ -0,0 +1,64 @@
+package ar.com.tutorialesya.proyecto008;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private EditText et1;
+    private int num;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1=(EditText)findViewById(R.id.editText);
+        num=(int)(Math.random()*100001);
+        String cadena=String.valueOf(num);
+        Toast notificacion=Toast.makeText(this,cadena,Toast.LENGTH_LONG);
+        notificacion.show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void controlar(View v) {
+        String valorIngresado=et1.getText().toString();
+        int valor=Integer.parseInt(valorIngresado);
+        if (valor==num) {
+            Toast notificacion=Toast.makeText(this,"Muy bien recordaste el número mostrado.",Toast.LENGTH_LONG);
+            notificacion.show();
+        }
+        else {
+            Toast notificacion=Toast.makeText(this,"Lo siento pero no es el número que mostré.",Toast.LENGTH_LONG);
+            notificacion.show();
+        }
+    }
+}
